3511fbeea24f8c0a506e4e00e9545b6680c76487
xudeng666 <1006543657@qq.com>
2025-06-11 21:53:33 +0800

    大冒险0.02
    游戏代码开始编写

--- std_dmx/server/server.cpp
+++ std_dmx/server/server.cpp
@@ -1,17 +1,24 @@
 #include "../thirdparty/httplib.h"
 
+#include <mutex>
+#include <string>
+#include <fstream>
+
+std::mutex g_mutex;//全局互斥锁
+std::string str_text;//文本内容
+
+int progress_1 = -1;//玩家1进度
+int progress_2 = -1;//玩家2进度
+
 int main(int argc, char** argv)
 {
-	httplib::Server server;
+	std::ifstream file("text.txt");
 
-	server.Post("/hello", [&](const httplib::Request& req, httplib::Response& res) 
-		{
-			std::cout << "Hello From Client!" << std::endl;
-
-			res.set_content("Hello From Server!", "text/plain");
-		});
-
-	server.listen("localhost", 25565);
+	if (!file.good())
+	{
+		MessageBox(nullptr, L"无法打开文本文件 text.txt", L"启动失败", MB_OK || MB_ICONERROR);
+		return -1;
+	}
 
 	return 0;
 }
