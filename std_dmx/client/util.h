5b2c17446da8b44d367aa5d58522c577c445888a
xudeng666 <1006543657@qq.com>
2025-06-16 22:01:40 +0800

    大冒险0.05
    定时器类、动画类和工具文件完成

---
+++ std_dmx/client/util.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Camera.h"
+
+#include <graphics.h>
+
+#pragma comment(lib,"WINMM.lib")
+#pragma commert(lib,"MSIMG32.lib")
+
+struct Rect
+{
+	int x, y;
+	int w, h;
+};
+
+inline void putimage_ex(const Camera& camera, IMAGE* img, const Rect* rect_dst, const Rect* rect_src = nullptr)
+{
+	static BLENDFUNCTION blend_func = { AC_SRC_OVER,0,255,AC_SRC_ALPHA };
+
+	const Vector2& pos_camera = camera.get_position();
+	AlphaBlend(GetImageHDC(GetWorkingImage()), (int)(rect_dst->x - pos_camera.x), (int)(rect_dst->y - pos_camera.y), 
+		rect_dst->w, rect_dst->h, GetImageHDC(img), rect_src ? rect_src->x : 0, rect_src ? rect_src->y : 0, 
+		rect_src ? rect_src->w : img->getwidth(), rect_src ? rect_src->h : img->getheight(), blend_func);
+}
+
+inline void load_audio(LPCSTR path, LPCSTR id)
+{
+    static TCHAR str_cmd[512];
+    _stprintf_s(str_cmd, _T("open %s alias %s"), path, id);
+    mciSendString(str_cmd, NULL, 0, NULL);
+}
+
+inline void play_audio(LPCSTR id, bool is_loop = false)
+{
+    static TCHAR str_cmd[512];
+    _stprintf_s(str_cmd, _T("play %s %s from 0"), id, is_loop ? _T("repeat") : _T(""));
+    mciSendString(str_cmd, NULL, 0, NULL);
+}
+
+inline void stop_audio(LPCSTR id)
+{
+    static TCHAR str_cmd[512];
+    _stprintf_s(str_cmd, _T("stop %s"), id);
+    mciSendString(str_cmd, NULL, 0, NULL);
+}
