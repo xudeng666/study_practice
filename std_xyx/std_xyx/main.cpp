391dde151396b2d02f5195625adf66510013bd4b
xudeng666 <1006543657@qq.com>
2025-08-11 11:21:31 +0800

    小游戏0.01
    导入SDL第三方库，搭建配置环境。

--- std_xyx/std_xyx/main.cpp
+++ std_xyx/std_xyx/main.cpp
@@ -1,5 +1,14 @@
 
-int main()
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+#include <chrono>
+#include <thread>
+
+int main(int argc, char* argv[])
 {
 	return 0;
 }
