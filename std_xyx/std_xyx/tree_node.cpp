1273980de0b63a30842f821dc1c94e06540f7848
xudeng666 <1006543657@qq.com>
2025-09-17 21:02:55 +0800

    小游戏0.30.0
    完善新的节点功能
    项目开启断言检查，方便调试

--- std_xyx/std_xyx/tree_node.cpp
+++ std_xyx/std_xyx/tree_node.cpp
@@ -1,1 +1,187 @@
 #include "tree_node.h"
+
+#include <assert.h>
+
+
+TreeNode::TreeNode(GameObj_UP d) : obj(std::move(d))
+{
+	assert(obj != nullptr && "GameObj cannot be null");
+}
+
+GameObj* TreeNode::get_obj()
+{
+	return obj.get();
+}
+
+void TreeNode::set_parent(TreeNode_SP p)
+{
+	parent = p;
+}
+
+TreeNode_SP TreeNode::get_parent()
+{
+	return parent.lock();
+}
+
+TreeNode_SP TreeNode::remove_children(GameObj* obj)
+{
+	auto it = find_child_iterator(obj);
+	if (it == children.end()) return nullptr;
+	TreeNode_SP removedObj = std::move(*it);
+	children.erase(it);
+	removedObj->parent.reset();
+	return removedObj;
+}
+
+TreeNode_SP TreeNode::remove_children(TreeNode_SP node)
+{
+	auto it = find_child_iterator(node);
+	if (it == children.end()) return nullptr;
+	TreeNode_SP removedObj = std::move(*it);
+	children.erase(it);
+	removedObj->parent.reset();
+	return removedObj;
+}
+
+TreeNode_SP TreeNode::remove_children(std::string id)
+{
+	auto it = find_child_iterator(id);
+	if (it == children.end()) return nullptr;
+	TreeNode_SP removedObj = std::move(*it);
+	children.erase(it);
+	removedObj->parent.reset();
+	return removedObj;
+}
+
+void TreeNode::delete_children(GameObj* obj)
+{
+	auto it = find_child_iterator(obj);
+	if (it != children.end())
+	{
+		(*it)->parent.reset();
+		children.erase(it);
+	}
+}
+
+void TreeNode::delete_children(TreeNode_SP node)
+{
+	auto it = find_child_iterator(node);
+	if (it != children.end())
+	{
+		(*it)->parent.reset();
+		children.erase(it);
+	}
+}
+void TreeNode::delete_children(std::string id)
+{
+	auto it = find_child_iterator(id);
+	if (it != children.end())
+	{
+		(*it)->parent.reset();
+		children.erase(it);
+	}
+}
+
+void TreeNode::add_children(TreeNode_SP node, bool is_front)
+{
+	if (!node)
+	{
+		assert(false && "Cannot add null node as child");
+		return;
+	}
+	if (!node->parent.expired()) {
+		node = node->get_parent()->remove_children(node);
+	}
+
+	// 步骤2：设置当前节点为新父节点
+	node->set_parent(shared_from_this());
+
+	// 步骤3：将子节点的 unique_ptr 移动到当前节点的 children 列表
+	is_front ? children.push_front(std::move(node)) : children.push_back(std::move(node));
+}
+
+void TreeNode::for_each_child(const std::function<void(GameObj*)>& func)
+{
+	if (!func) return;
+
+	for (const auto& child_ptr : children) {
+		if (child_ptr) {
+			func(child_ptr->get_obj());
+		}
+	}
+}
+
+void TreeNode::sort_children(const std::function<bool(const TreeNode_SP&, const TreeNode_SP&)>& func)
+{
+	if (!func) return;
+
+	children.sort([&func](const TreeNode_SP& a, const TreeNode_SP& b) {
+		// 如果有空节点，则排在后面
+		if (!a) return false;
+		if (!b) return true;
+		// 解引用智能指针，调用外部传入的比较逻辑
+		return func(a, b);
+		});
+}
+
+void TreeNode::remove_children_if(const std::function<bool(const TreeNode_SP&)>& func)
+{
+	if (!func) return;
+
+	children.remove_if([&func](const TreeNode_SP& child) {
+		if (!child)
+		{
+			return true;
+		}
+		return func(child);
+		});
+}
+
+void TreeNode::clear_children()
+{
+	children.clear();
+}
+
+int TreeNode::get_children_size()
+{
+	return children.size();
+}
+
+TreeNode_SP TreeNode::find_child(GameObj* obj)
+{
+	auto it = find_child_iterator(obj);
+	if (it != children.end() && *it) {
+		return *it;
+	}
+	return nullptr;
+}
+
+TreeNode_SP TreeNode:: find_child(const std::string& id)
+{
+	auto it = find_child_iterator(id);
+	if (it != children.end() && *it) {
+		return *it;
+	}
+	return nullptr;
+}
+
+TreeNode::ChildIt TreeNode:: find_child_iterator(GameObj* obj)
+{
+	return std::find_if(children.begin(), children.end(),
+		[obj](const TreeNode_SP& child) {
+			return child->get_obj() == obj;
+		});
+}
+
+TreeNode::ChildIt TreeNode:: find_child_iterator(TreeNode_SP node)
+{
+	return std::find(children.begin(), children.end(), node);
+}
+
+TreeNode::ChildIt TreeNode:: find_child_iterator(const std::string& id)
+{
+	return std::find_if(children.begin(), children.end(),
+		[id](const TreeNode_SP& child) {
+			return child->get_obj()->get_ID() == id;
+		});
+}
--- std_xyx/std_xyx/tree_node.h
+++ std_xyx/std_xyx/tree_node.h
@@ -6,40 +6,45 @@
 #include <list>
 
 
+class TreeNode;
 
-class TreeNode
+typedef std::shared_ptr<TreeNode> TreeNode_SP;      // 节点指针（强）
+typedef std::unique_ptr<GameObj> GameObj_UP;        // 对象指针（专）
+typedef std::weak_ptr<TreeNode> TreeNode_WP;        // 节点指针（弱）
+
+class TreeNode:public std::enable_shared_from_this<TreeNode>
 {
 public:
 	//构造
-	TreeNode(std::unique_ptr<GameObj> d) : obj(std::move(d)) {}
+    TreeNode(GameObj_UP d);
 	//析构
 	~TreeNode() = default;
+    // 禁用拷贝
+    TreeNode(const TreeNode&) = delete;
+    TreeNode& operator=(const TreeNode&) = delete;
+    // 允许移动
+    TreeNode(TreeNode&&) = default;
+    TreeNode& operator=(TreeNode&&) = default;
 	//获取数据
-	GameObj* get_obj()
-	{
-		return obj.get();
-	}
-
+    GameObj* get_obj();
     /*设置父节点*/
-    void set_parent(TreeNodePtr p);
+    void set_parent(TreeNode_SP p);
     /*获取父节点*/
-    TreeNodePtr get_parent();
+    TreeNode_SP get_parent();
     /*移除(不删除)子节点*/
-    TreeNodePtr remove_children(GameObj* obj);
-    TreeNodePtr remove_children(ObjPtr obj);
-    TreeNodePtr remove_children(TreeNodePtr node);
-    TreeNodePtr remove_children(std::string id);
+    TreeNode_SP remove_children(GameObj* obj);
+    TreeNode_SP remove_children(TreeNode_SP node);
+    TreeNode_SP remove_children(std::string id);
     /*删除子节点*/
     void delete_children(GameObj* obj);
-    void delete_children(ObjPtr obj);
-    void delete_children(TreeNodePtr node);
+    void delete_children(TreeNode_SP node);
     void delete_children(std::string id);
     /// <summary>
     /// 添加子节点（默认表尾添加）
     /// </summary>
     /// <param name="obj">节点智能指针</param>
     /// <param name="is_front">true/false 是否表头添加 默认false</param>
-    void add_children(TreeNodePtr node, bool is_front = false);
+    void add_children(TreeNode_SP node, bool is_front = false);
     /// <summary>
     /// 遍历所有子对象
     /// </summary>
@@ -49,12 +54,12 @@ public:
     /// 子节点排序
     /// </summary>
     /// <param name="func">排序顺序函数</param>
-    void sort_children(const std::function<bool(const TreeNodePtr&, const TreeNodePtr&)>& func);
+    void sort_children(const std::function<bool(const TreeNode_SP&, const TreeNode_SP&)>& func);
     /// <summary>
     /// 删除符合条件的子节点
     /// </summary>
     /// <param name="func"></param>
-    void remove_children_if(const std::function<bool(const TreeNodePtr&)>& func);
+    void remove_children_if(const std::function<bool(const TreeNode_SP&)>& func);
     /// <summary>
     /// 清理子节点
     /// </summary>
@@ -64,13 +69,41 @@ public:
     /// </summary>
     /// <returns>int</returns>
     int get_children_size();
+    /// <summary>
+    /// 查找子节点（直接）
+    /// </summary>
+    /// <param name="obj">GameObj*</param>
+    /// <returns>TreeNode_SP</returns>
+    TreeNode_SP find_child(GameObj* obj);
+    /// <summary>
+    /// 查找子节点（直接）
+    /// </summary>
+    /// <param name="id">string id</param>
+    /// <returns>TreeNode_SP</returns>
+    TreeNode_SP find_child(const std::string& id);
+private:
+    using ChildIt = std::list<TreeNode_SP>::iterator;
+    /// <summary>
+    /// 查找子节点it（直接）
+    /// </summary>
+    /// <param name="obj">GameObj*</param>
+    /// <returns>ChildIt</returns>
+    ChildIt find_child_iterator(GameObj* obj);
+    /// <summary>
+    /// 查找子节点it（直接）
+    /// </summary>
+    /// <param name="node">TreeNode_SP</param>
+    /// <returns>ChildIt</returns>
+    ChildIt find_child_iterator(TreeNode_SP node);
+    /// <summary>
+    /// 查找子节点it（直接）
+    /// </summary>
+    /// <param name="id">string id</param>
+    /// <returns>ChildIt</returns>
+    ChildIt find_child_iterator(const std::string& id);
 
 private:
-	ObjPtr obj;
-    ParentPtr parent;
-	std::list<TreeNodePtr> children;
+	GameObj_UP obj;
+    TreeNode_WP parent;
+	std::list<TreeNode_SP> children;
 };
-
-typedef std::shared_ptr<TreeNode> TreeNodePtr;
-typedef std::unique_ptr<GameObj> ObjPtr;
-typedef std::weak_ptr<GameObj> ParentPtr;
